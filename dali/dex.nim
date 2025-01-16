5576e67f2ca452e54a3811a243e2205c69b16adb
Andreas Tzionis <andy0n7@gmail.com>
2025-01-16 11:15:58 +0200

    minor fix

--- dali/dex.nim
+++ dali/dex.nim
@@ -8,7 +8,6 @@ import tables
 import patty
 
 import blob
-import sortedset
 import types
 
 # Potentially useful bibliography
@@ -48,6 +47,37 @@ converter toUint32[T: enum](s: set[T]): uint32 =
     result = result or v.ord.uint32
 #########
 #########
+
+
+type SortedSet*[T] = distinct seq[T]
+
+proc init*[T](s: var SortedSet[T]) {.inline.} = s = SortedSet[T](newSeq[T]())
+
+proc incl*[T](s: var SortedSet[T], item: T) =
+  let i = s.search(item)
+  if i == s.len:
+    seq[T](s).add(item)
+  elif item < s[i]:
+    seq[T](s).insert(item, i)
+
+proc `[]`*[T](s: SortedSet[T], i: int): T {.inline.} = seq[T](s)[i]
+
+proc len*[T](s: SortedSet[T]): int {.inline.} = seq[T](s).len
+
+proc search*[T](s: SortedSet[T], item: T): int =
+  var i = s.len
+  while result < i:
+    let mid = (result + i) shr 1
+    if item < s[mid]:
+      i = mid
+    elif s[mid] < item:
+      result = mid + 1
+    else:
+      return mid
+
+iterator items*[T](s: SortedSet[T]): T =
+  for item in seq[T](s):
+    yield item
 
 proc newDex*(): Dex =
   new(result)
