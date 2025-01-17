ec3ff5e8455238b91d0d3813b426ddeac7fdd6c6
Andreas Tzionis <andy0n7@gmail.com>
2025-01-17 08:57:10 +0200

    restore instrs

--- nim/dex.nim
+++ nim/dex.nim
@@ -608,3 +608,57 @@ proc render*(dex: Dex): string =
   # stderr.write("\n")
 
   return blob.string
+
+
+
+proc newInstr*(opcode: uint8, args: varargs[Arg]): Instr =
+  return Instr(opcode: opcode, args: @args)
+
+proc newInvoke1*(opcode: uint8, regC: uint4, m: Method): Instr =
+  return newInstr(opcode, RawX(1), RawX(0), MethodXXXX(m), RawX(0), RegX(regC), RawXX(0))
+proc newInvoke2*(opcode: uint8, regC: uint4, regD: uint4, m: Method): Instr =
+  return newInstr(opcode, RawX(2), RawX(0), MethodXXXX(m), RawX(regD), RegX(regC), RawXX(0))
+
+proc move_result_object*(reg: uint8): Instr =
+  return newInstr(0x0c, RegXX(reg))
+
+proc return_void*(): Instr =
+  return newInstr(0x0e, RawXX(0))
+proc return_object*(reg: uint8): Instr =
+  return newInstr(0x11, RawXX(reg))
+
+proc const_high16*(reg: uint8, highBits: uint16): Instr =
+  return newInstr(0x15, RegXX(reg), RawXXXX(highBits))
+proc const_wide_16*(regs: uint8, v: int16): Instr =
+  return newInstr(0x16, RegXX(regs), RawXXXX(v.uint16))
+proc const_string*(reg: uint8, s: String): Instr =
+  return newInstr(0x1a, RegXX(reg), StringXXXX(s))
+
+proc new_instance*(reg: uint8, t: Type): Instr =
+  return newInstr(0x22, RegXX(reg), TypeXXXX(t))
+
+proc iget_wide*(regsA: uint4, regB: uint4, field: Field): Instr =
+  return newInstr(0x53, RegX(regB), RegX(regsA), FieldXXXX(field))
+proc iput_wide*(regsA: uint4, regB: uint4, field: Field): Instr =
+  return newInstr(0x5a, RegX(regB), RegX(regsA), FieldXXXX(field))
+
+proc sget_object*(reg: uint8, field: Field): Instr =
+  return newInstr(0x62, RegXX(reg), FieldXXXX(field))
+
+proc invoke_virtual*(regC: uint4, m: Method): Instr =
+  return newInvoke1(0x6e, regC, m)
+proc invoke_virtual*(regC: uint4, regD: uint4, m: Method): Instr =
+  return newInvoke2(0x6e, regC, regD, m)
+
+proc invoke_super*(regC: uint4, regD: uint4, m: Method): Instr =
+  return newInvoke2(0x6f, regC, regD, m)
+
+proc invoke_direct*(regC: uint4, m: Method): Instr =
+  return newInvoke1(0x70, regC, m)
+proc invoke_direct*(regC: uint4, regD: uint4, m: Method): Instr =
+  return newInvoke2(0x70, regC, regD, m)
+
+proc invoke_static*(regC: uint4, m: Method): Instr =
+  return newInvoke1(0x71, regC, m)
+proc invoke_static*(regC, regD: uint4, m: Method): Instr =
+  return newInvoke2(0x71, regC, regD, m)
--- nim/main.nim
+++ nim/main.nim
@@ -6,29 +6,6 @@ include hex
 # nimble install
 # nim compile -f --run nim/main.nim
 
-proc newInstr*(opcode: uint8, args: varargs[Arg]): Instr = return Instr(opcode: opcode, args: @args)
-
-proc return_void*(): Instr = return newInstr(0x0e, RawXX(0))
-proc const_string*(reg: uint8, s: String): Instr = return newInstr(0x1a, RegXX(reg), StringXXXX(s))
-proc sget_object*(reg: uint8, field: Field): Instr = return newInstr(0x62, RegXX(reg), FieldXXXX(field))
-proc invoke_virtual*(regC: dex.uint4, regD: dex.uint4, m: Method): Instr =
-  return newInstr(0x6e, RawX(2), RawX(0), MethodXXXX(m), RawX(regD), RegX(regC), RawXX(0))
-
-proc newInvoke1*(opcode: uint8, regC: uint4, m: Method): Instr =
-  return newInstr(opcode, RawX(1), RawX(0), MethodXXXX(m), RawX(0), RegX(regC), RawXX(0))
-
-proc invoke_direct*(regC: uint4, m: Method): Instr =
-  return newInvoke1(0x70, regC, m)
-
-proc newInvoke2*(opcode: uint8, regC: uint4, regD: uint4, m: Method): Instr =
-  return newInstr(opcode, RawX(2), RawX(0), MethodXXXX(m), RawX(regD), RegX(regC), RawXX(0))
-
-proc invoke_super*(regC: uint4, regD: uint4, m: Method): Instr =
-  return newInvoke2(0x6f, regC, regD, m)
-
-proc const_high16*(reg: uint8, highBits: uint16): Instr =
-  return newInstr(0x15, RegXX(reg), RawXXXX(highBits))
-
 let dex2 = newDex()
 #-- Prime some arrays, to make sure their order matches hello_android_apk
 dex2.addStr"<init>"
